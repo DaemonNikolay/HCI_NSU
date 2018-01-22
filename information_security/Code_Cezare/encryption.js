94557ab07d90056e9312485b7e0d26103ecb8776
DaemonNikolay <nikolay-eckert@mail.ru>
2018-01-22 17:07:59 +0700

    Update code Cezare

--- information_security/Code_Cezare/encryption.js
+++ information_security/Code_Cezare/encryption.js
@@ -1,56 +1,69 @@
-var messageBox = document.search.message;
-var keyCodeCezare = document.search.valueOffset;
+(function() {
 
-async function onkeypress(e) {
+    var messageBox = document.search.message;
+    var keyCodeCezare = document.search.valueOffset;
 
-    await sleep(10);
+    async function onkeypress(e) {
 
-    var outBlock = document.getElementById('out');
-    var offset = Number(document.getElementById('valueOffset').value);
-    var content = document.getElementsByName("message")[0].value;
+        await sleep(10);
 
-    outBlock.textContent = "";
+        var offset = Number(document.getElementById('valueOffset').value);
+        var outBlock = document.getElementById('out');
 
-    var result = "";
-    for (var i = 0; i < content.length; i++) {
-        if (content[i] == ".") {
-            result += convertSymbol(0, content[i]);
-        } else if (content[i] == ",") {
-            result += convertSymbol(0, content[i]);
-        } else if (content[i] == "ё") {
-            result += convertSymbol(offset, "е");
-        } else if (content[i] == " ") {
-            result += convertSymbol(0, content[i]);
-        } else if (content[i] == "!") {
-            result += convertSymbol(0, content[i]);
-        } else if (content[i] == "?") {
-            result += convertSymbol(0, content[i]);
-        } else if (content[i] == "\n") {
-            result += convertSymbol(0, content[i]);
+        if (Number(offset) <= 32 && Number(offset) >= 0) {
+            convertText(outBlock, offset);
         } else {
-            result += convertSymbol(offset, content[i]);
+            outBlock.textContent = "error! max offset 32!";
         }
     }
-    outBlock.textContent = result;
-}
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function convertSymbol(step, symbol) {
-    var value = String.fromCodePoint((String(symbol).charCodeAt() + step));
-    if (value > "я") {
-        return String.fromCodePoint((String(symbol).charCodeAt() + step - 32));
-    } else if (value > "z" && value < "а" && step <= 26) {
-        return String.fromCodePoint((String(symbol).charCodeAt() + step - 26));
-    } else if (value > "z" && value < "а" && step > 26) {
-        console.log(1);
-        return String.fromCodePoint((String(symbol).charCodeAt() + (step - 25) - 26));
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
-    return value;
-}
 
-messageBox.addEventListener("keypress", onkeypress);
-messageBox.addEventListener("keydown", onkeypress);
-keyCodeCezare.addEventListener("keydown", onkeypress);
+    function convertText(outBlock, offset) {
+
+        var content = document.getElementsByName("message")[0].value;
+
+        outBlock.textContent = "";
+        var result = "";
+        for (var i = 0; i < content.length; i++) {
+            if (content[i] == ".") {
+                result += convertSymbol(0, content[i]);
+            } else if (content[i] == ",") {
+                result += convertSymbol(0, content[i]);
+            } else if (content[i] == "ё") {
+                result += convertSymbol(offset, "е");
+            } else if (content[i] == " ") {
+                result += convertSymbol(0, content[i]);
+            } else if (content[i] == "!") {
+                result += convertSymbol(0, content[i]);
+            } else if (content[i] == "?") {
+                result += convertSymbol(0, content[i]);
+            } else if (content[i] == "\n") {
+                result += convertSymbol(0, content[i]);
+            } else {
+                result += convertSymbol(offset, content[i]);
+            }
+        }
+        outBlock.textContent = result;
+    }
+
+    function convertSymbol(step, symbol) {
+        var value = String.fromCodePoint((String(symbol).charCodeAt() + step));
+        if (value > "я") {
+            return String.fromCodePoint((String(symbol).charCodeAt() + step - 32));
+        } else if (value > "z" && value < "а" && step <= 26) {
+            return String.fromCodePoint((String(symbol).charCodeAt() + step - 26));
+        } else if (value > "z" && value < "а" && step > 26) {
+            return String.fromCodePoint((String(symbol).charCodeAt() + (step - 25) - 26));
+        }
+        return value;
+    }
+
+    messageBox.addEventListener("keypress", onkeypress);
+    messageBox.addEventListener("keydown", onkeypress);
+    keyCodeCezare.addEventListener("keydown", onkeypress);
+    keyCodeCezare.addEventListener("click", onkeypress);
+
+})();
