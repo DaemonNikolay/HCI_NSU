1d813aee96b78d37f6dd99d8a37f010398171882
DaemonNikolay <nikolay-eckert@mail.ru>
2018-02-20 12:05:02 +0700

    Update for report

--- information_security/Encoder_randomly/decoder/source.py
+++ information_security/Encoder_randomly/decoder/source.py
@@ -1,46 +1,69 @@
+def stylingSymbolStepOne(symbol):
+    return "\x1b[31m" + str(symbol) + "\x1b[0m"
+
+
+def stylingSymbolStepTwo(symbol):
+    return "\x1b[34m" + str(symbol) + "\x1b[0m"
+
+
+def stylingSymbolStepThree(symbol):
+    return "\x1b[32m" + str(symbol) + "\x1b[0m"
+
+
 file_input = open("text.txt", "r", encoding = "UTF8")
 line = file_input.readline()
 
-code = {"з": "п",  # шаг 1
-        "в": "е",
-        "б": "р",
-        "с": "в",
-        "д": "о",
-        "а": "с",
-        "ю": "и",
-        "р": "т",
-        "у": "ы",
-        "и": "а",
-        "п": "ь",
-        "л": "к",  # шаг 2
-        "ё": "л",
-        "ч": "х",
-        "ж": "ш",
-        "ц": "ю",
-        "г": "я",
-        "щ": "д",
-        "к": "м",
-        "о": "ч",
-        "м": "н",
-        "е": "у",
-        "ш": "ж",  # шаг 3
-        "й": "й",
-        "ф": "г",
-        "ъ": "з",
-        "я": "б",
-        "х": "э",
+code = {"з": stylingSymbolStepOne("п"),  # шаг 1
+        "в": stylingSymbolStepOne("е"),
+        "б": stylingSymbolStepOne("р"),
+        "с": stylingSymbolStepOne("в"),
+        "д": stylingSymbolStepOne("о"),
+        "а": stylingSymbolStepOne("с"),
+        "ю": stylingSymbolStepOne("и"),
+        "р": stylingSymbolStepOne("т"),
+        "у": stylingSymbolStepOne("ы"),
+        "и": stylingSymbolStepOne("а"),
+        "п": stylingSymbolStepOne("ь"),
+        "л": stylingSymbolStepTwo("к"),  # шаг 2
+        "ё": stylingSymbolStepTwo("л"),
+        "ч": stylingSymbolStepTwo("х"),
+        "ж": stylingSymbolStepTwo("ш"),
+        "ц": stylingSymbolStepTwo("ю"),
+        "г": stylingSymbolStepTwo("я"),
+        "щ": stylingSymbolStepTwo("д"),
+        "к": stylingSymbolStepTwo("м"),
+        "о": stylingSymbolStepTwo("ч"),
+        "м": stylingSymbolStepTwo("н"),
+        "е": stylingSymbolStepTwo("у"),
+        "ш": stylingSymbolStepThree("ж"),  # шаг 3
+        "й": stylingSymbolStepThree("й"),
+        "ф": stylingSymbolStepThree("г"),
+        "ъ": stylingSymbolStepThree("з"),
+        "я": stylingSymbolStepThree("б"),
+        "х": stylingSymbolStepThree("э"),
         }
+
+encodeText = []
 print()
 while line:
     for element in line:
         if element in code:
-            print("\x1b[31m" + code[element] + "\x1b[0m", end = "")
+            # print("\x1b[31m" + code[element] + "\x1b[0m", end = "")
+            # print(code[element], end = "")
+            encodeText += code[element]
+        elif (element == ','):
+            encodeText += element + "\n"
         else:
-            print(element, end = "")
+            encodeText += element
+            # print(element, end = "")
     line = file_input.readline()
 print()
 file_input.close()
 
+result = "".join(encodeText).split(" ")
+for element in result:
+    print(element, end = " ")
+print()
 
 # перевоспитывать
 # звбвсдазюрусирп
