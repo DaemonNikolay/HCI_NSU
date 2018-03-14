aa31ed1be03626bd6850fb83207b26c9024d191c
DaemonNikolay <nikolay-eckert@mail.ru>
2018-03-15 01:37:52 +0700

    Create Randomly

---
+++ information_security/Four_encode_algorithm_WEB_GUI/js/Encoding/Randomly.js
@@ -0,0 +1,71 @@
+(function() {
+
+    var inputMessage = document.Randomly.InputTextRandomly;
+
+
+    async function onkeypress(e) {
+        await Sleep(10);
+
+        var message = document.getElementById('InputTextRandomly').value;
+
+        var baseAlpabet = CreateAlphabet(message);
+        var shuffleAlphabet = ShuffleArray(Object.assign([], baseAlpabet));
+
+        var output = document.getElementById('OutputTextRandomly');
+
+        RandomlyText(shuffleAlphabet, baseAlpabet, message, output);
+    }
+
+    function RandomlyText(shuffleAlphabet, baseAlpabet, message, output) {
+
+        var result = "";
+
+        for (i = 0; i < message.length; i++) {
+            if (message[i] == " ") {
+                result += message[i];
+                continue;
+            }
+            var numAlph = baseAlpabet.indexOf(message[i]);
+            result += shuffleAlphabet[numAlph];
+        }
+
+        output.textContent = result;
+    }
+
+    function CreateAlphabet(text) {
+        text = String(text).replace(/[ ]|[.]|[,]|[!]|[*]|[?]|[//]|[-]|[0-9]/g, "").split("");
+
+        return Unique(text);
+    }
+
+    function ShuffleArray(array) {
+
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        return array;
+    }
+
+    function Unique(arr) {
+
+        var obj = {};
+        for (var i = 0; i < arr.length; i++) {
+            var str = arr[i];
+            obj[str] = true;
+        }
+
+        return Object.keys(obj);
+    }
+
+    function Sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    inputMessage.addEventListener("keypress", onkeypress);
+    inputMessage.addEventListener("keydown", onkeypress);
+
+})();
