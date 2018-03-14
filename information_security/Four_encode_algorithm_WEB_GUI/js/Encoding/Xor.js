70775aea5445a6fd841d1308ab48b204157acb3e
DaemonNikolay <nikolay-eckert@mail.ru>
2018-03-14 23:20:56 +0700

    Create encoding XOR

--- information_security/Four_encode_algorithm_WEB_GUI/js/Encoding/Xor.js
+++ information_security/Four_encode_algorithm_WEB_GUI/js/Encoding/Xor.js
@@ -1,1 +1,77 @@
-console.log("This is XOR");
+(function() {
+
+    var inputMessage = document.xorEncrypt.InputXor;
+    var key = document.xorEncrypt.KeyXor;
+
+    async function onkeypress(e) {
+
+        await Sleep(10);
+
+        var outputXor = document.getElementById('OutputXor');
+        var offset = Number(document.getElementById('KeyXor').value);
+
+        if (ValidKey(offset) == false) {
+            outputXor.textContent = "Ошибка ввода!";
+        } else {
+            ValidInput() == true ? EncodingXor(outputXor, offset) : outputXor.textContent = "Ошибка ввода!";
+        }
+
+    }
+
+    function EncodingXor(output, key) {
+
+        var content = document.getElementsByName("InputXor")[0].value;
+        var result = "";
+        var j = 0;
+        key = String(key);
+
+        for (i = 0; i < content.length; i++) {
+            result += content[i] ^ Number(key[j]);
+
+            j++;
+            if (j == key.length) {
+                j = 0;
+            }
+        }
+
+        output.textContent = result;
+    }
+
+    function ValidKey(key) {
+
+        key = String(key);
+        for (i = 0; i < key.length; i++) {
+            if (key[i] > 1 || key[i] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function ValidInput() {
+
+        var content = document.getElementsByName("InputXor")[0].value;
+        if (isNaN(Number(content))) {
+            return false;
+        }
+
+        for (i = 0; i < content.length; i++) {
+            if (content[i] > 1 || content[i] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function Sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    inputMessage.addEventListener("keypress", onkeypress);
+    inputMessage.addEventListener("keydown", onkeypress);
+    key.addEventListener("keydown", onkeypress);
+    key.addEventListener("click", onkeypress);
+
+})();
