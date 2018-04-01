dacfa0403b3094e54c095dc9f21b09d3da2e57c1
DaemonNikolay <nikolay-eckert@mail.ru>
2018-04-02 01:27:02 +0700

    Upgrade item 'combo encoding'

--- information_security/Four_encode_algorithm_WEB_GUI/js/Encoding/ComboEncoding.js
+++ information_security/Four_encode_algorithm_WEB_GUI/js/Encoding/ComboEncoding.js
@@ -1,16 +1,22 @@
 (function() {
 
-    var SelectEncoding0 = document.ComboEncoding.SelectValueSequenceEncoding0;
-    // var SelectEncoding1 = document.ComboEncoding.SelectValueSequenceEncoding;
-    // var SelectEncoding2 = document.ComboEncoding.SelectValueSequenceEncoding;
+    var SelectEncodingZero = document.ComboEncoding.SelectValueSequenceEncodingZero;
+    var SelectEncodingOne = document.ComboEncoding.SelectValueSequenceEncodingOne;
+    var SelectEncodingTwo = document.ComboEncoding.SelectValueSequenceEncodingTwo;
 
 
     async function onkeypress(e) {
         await sleep(20);
 
-        var value = document.getElementById("SelectValueSequenceEncoding0").value;
+        var valueSelectZero = document.getElementById("SelectValueSequenceEncodingZero").value;
+        var valueSelectOne = document.getElementById("SelectValueSequenceEncodingOne").value;
+        var valueSelectTwo = document.getElementById("SelectValueSequenceEncodingTwo").value;
 
-        alert(value);
+        console.log(valueSelectZero + "\n" + valueSelectOne + '\n' + valueSelectTwo + "\n\n");
+
+
+
+
     }
 
 
@@ -22,8 +28,7 @@
 
 
 
-    SelectEncoding0.addEventListener("change", onkeypress);
-    // SelectEncoding1.addEventListener("change", onkeypress);
-    // SelectEncoding2.addEventListener("change", onkeypress);
-
+    SelectEncodingZero.addEventListener("change", onkeypress);
+    SelectEncodingOne.addEventListener("change", onkeypress);
+    SelectEncodingTwo.addEventListener("change", onkeypress);
 })();
