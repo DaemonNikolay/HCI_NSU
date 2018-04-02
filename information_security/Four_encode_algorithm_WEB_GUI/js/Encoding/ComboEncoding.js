540692bdb8dbbf5539b90aeccbf0a254121ad733
DaemonNikolay <nikolay-eckert@mail.ru>
2018-04-02 11:08:32 +0700

    Create combo-encoding (non-stable)

--- information_security/Four_encode_algorithm_WEB_GUI/js/Encoding/ComboEncoding.js
+++ information_security/Four_encode_algorithm_WEB_GUI/js/Encoding/ComboEncoding.js
@@ -21,12 +21,46 @@
 
         // console.log(valueSelectZero + "\n" + valueSelectOne + '\n' + valueSelectTwo + "\n\n");
 
-        var inputCombo = document.getElementById("InputTextCombo").value;
-        var outputCombo = document.getElementById("OutputTextCombo").value;
+        var inputCombo = document.getElementById("InputTextCombo").textContent;
+        var outputCombo = document.getElementById("OutputTextCombo");
+
 
         // console.log(inputCombo + "\t" + outputCombo);
 
+        var lengthBlockPermutation = document.getElementById("LengthBlockPermutation").value;
+        var keyBlockPermutation = document.getElementById("KeyBlockPermutation").value;
 
+        var offsetCezare = document.getElementById("ComboValueOffsetCezare").value;
+
+
+        var firstEncoding;
+        if (valueSelectZero == 0) {
+            firstEncoding = BlockPermutationEncoding(inputCombo, lengthBlockPermutation, keyBlockPermutation);
+        } else if (valueSelectZero == 1) {
+            firstEncoding = Randomly(inputCombo);
+        } else if (valueSelectZero == 2) {
+            firstEncoding = Cezare(offsetCezare, inputCombo);
+        }
+
+        var secondEncoding;
+        if (valueSelectOne == 0) {
+            secondEncoding = BlockPermutationEncoding(firstEncoding, lengthBlockPermutation, keyBlockPermutation);
+        } else if (valueSelectOne == 1) {
+            secondEncoding = Randomly(firstEncoding);
+        } else if (valueSelectOne == 2) {
+            secondEncoding = Cezare(offsetCezare, firstEncoding);
+        }
+
+        var thirdEncoding;
+        if (valueSelectTwo == 0) {
+            thirdEncoding = BlockPermutationEncoding(secondEncoding, lengthBlockPermutation, keyBlockPermutation);
+        } else if (valueSelectTwo == 1) {
+            thirdEncoding = Randomly(secondEncoding);
+        } else if (valueSelectTwo == 2) {
+            thirdEncoding = Cezare(offsetCezare, secondEncoding);
+        }
+
+        outputCombo.textContent = thirdEncoding;
     }
 
 
@@ -36,27 +70,178 @@
     }
 
 
+    //Block permutation
+
+    function BlockPermutationEncoding(content, lengthBox, stirringKey) {
+
+        stirringKey = ConvertNumberProgrammistToUser(stirringKey);
+
+        var lengthKey = stirringKey.toString().length;
+
+        var blocksText = DivideTextBlocks(content, lengthBox);
+        var shuffleText = ShuffleText(blocksText, stirringKey);
+
+        return shuffleText;
+    }
+
+    function ConvertNumberProgrammistToUser(number) {
+
+        number = number.toString();
+        var result = "";
+        for (var i = 0; i < number.length; i++) {
+            result += Number(number[i]) - 1;
+        }
+
+        return Number(result);
+    }
+
+    function IsCorrectStirringKey(lengthBox, key) {
+
+        key = key.toString();
+        for (var i = 0; i < key.length; i++) {
+            if (key[i] >= lengthBox) {
+                return false;
+            }
+        }
+
+        if (!KeyUnique(key)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    function KeyUnique(key) {
+
+        key = key.toString();
+        var n = key.length;
+        for (var i = 0; i < n - 1; i++) {
+            for (var j = i + 1; j < n; j++) {
+                if (key[i] === key[j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    function DivideTextBlocks(baseText, numberOfBlocks) {
+        baseText = String(baseText);
+
+        while (baseText.length % numberOfBlocks != 0) {
+            baseText += " ";
+        }
+
+        var blocksText = [];
+        var startingPoint = 0;
+        while (startingPoint != baseText.length) {
+            blocksText.push(baseText.substring(startingPoint, numberOfBlocks + startingPoint));
+            startingPoint += numberOfBlocks;
+        }
+
+        return blocksText;
+    }
+
+    function ShuffleText(blocksText, key) {
+
+        var result = [];
+        key = String(key);
+
+        blocksText.forEach(element => {
+            var temp = [];
+            for (var i = 0; i < key.length; i++) {
+                temp.push(element[key[i]]);
+            }
+
+            result.push(temp.join(""));
+        });
+
+        return result.join("");
+    }
+
+
+    //Randomly
+
+    function Randomly(inputText) {
+
+        var message = document.getElementById(inputText);
+
+        var baseAlpabet = CreateAlphabet(message);
+        var shuffleAlphabet = ShuffleArray(Object.assign([], baseAlpabet));
+
+        return RandomlyText(shuffleAlphabet, baseAlpabet, message);
+    }
+
+    function RandomlyText(shuffleAlphabet, baseAlpabet, message) {
+
+        var result = "";
+        var specialSymbols = [",", ".", "!", "?", "\\", "!", "/", "|", " ", "   "];
+
+        for (i = 0; i < message.length; i++) {
+            if (specialSymbols.indexOf(message[i]) != -1) {
+                result += message[i];
+                continue;
+            }
+            var numAlph = baseAlpabet.indexOf(message[i]);
+            result += shuffleAlphabet[numAlph];
+        }
+
+        return result;
+    }
+
+    function CreateAlphabet(text) {
+        text = String(text).replace(/[ ]|[.]|[,]|[!]|[*]|[?]|[//]|[-]|[0-9]/g, "").split("");
+
+        return Unique(text);
+    }
+
+    function ShuffleArray(array) {
+
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        return array;
+    }
+
+    function Unique(arr) {
+
+        var obj = {};
+        for (var i = 0; i < arr.length; i++) {
+            var str = arr[i];
+            obj[str] = true;
+        }
+
+        return Object.keys(obj);
+    }
+
+
 
     //Cezare Encoding
 
-    function validData(valueOffset, Output) {
-        var offset = Number(document.getElementById(valueOffset).value);
+    function Cezare(offset, content) {
+
+        return cezareValidData(offset, content);
+    }
+
+    function cezareValidData(offset, content) {
+
+        var offset = Number(offset);
         if (offset > 32) {
             offset = offset % 32;
         } else if (offset < -32) {
             offset = -(offset % 32);
         }
 
-        var OutputCezare = document.getElementById(Output);
-
-        convertText(OutputCezare, offset);
+        return convertText(offset, content);
     }
 
-    function convertText(OutputCezare, offset, inputText) {
+    function convertText(offset, content) {
 
-        var content = document.getElementsByName(inputText)[0].value;
-
-        OutputCezare.textContent = "";
         var result = "";
 
         for (var i = 0; i < content.length; i++) {
@@ -79,7 +264,7 @@
             }
         }
 
-        OutputCezare.textContent = result;
+        return result;
     }
 
     function convertSymbol(step, symbol) {
@@ -115,6 +300,7 @@
 
         return flag == false ? value : value.toUpperCase();
     }
+
 
 
 
