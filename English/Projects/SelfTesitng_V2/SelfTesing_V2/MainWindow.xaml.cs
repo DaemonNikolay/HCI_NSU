f4a06aa8a76368d3ff6204fc8988b36c81c7bd9f
DaemonNikolay <nikolay-eckert@mail.ru>
2018-04-12 00:27:05 +0700

    Create validation writting fields

--- English/Projects/SelfTesitng_V2/SelfTesing_V2/MainWindow.xaml.cs
+++ English/Projects/SelfTesitng_V2/SelfTesing_V2/MainWindow.xaml.cs
@@ -23,13 +23,115 @@ namespace SelfTesing_V2
         public MainWindow()
         {
             InitializeComponent();
-
-            Loaded += MainWindow_Loaded;
         }
 
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        private void CreateFileWithAnswers_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (IsAllFieldsFilled())
+            {
+                MessageBoxButton buttonMessageBox = MessageBoxButton.YesNo;
+                MessageBoxImage iconMessageBox = MessageBoxImage.Question;
+                MessageBoxResult resultMessageBox = MessageBox.Show("Уверены в своих результатах?", "Кря-кря", buttonMessageBox, iconMessageBox);
+
+                if (resultMessageBox == MessageBoxResult.Yes)
+                {
+                    MessageBox.Show("" + (ComboBox_task_1_1.SelectedValue == null));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не все поля заполнены!");
+            }
+        }
+
+        private bool IsAllFieldsFilled()
+        {
+            if (ComboBox_task_1_1.SelectedValue == null)
+            {
+                return false;
+            }
+            else if (ComboBox_task_1_2.SelectedValue == null)
+            {
+                return false;
+            }
+            else if (ComboBox_task_1_2.SelectedValue == null)
+            {
+                return false;
+            }
+            else if (ComboBox_task_1_3.SelectedValue == null)
+            {
+                return false;
+            }
+            else if (ComboBox_task_1_4.SelectedValue == null)
+            {
+                return false;
+            }
+            else if (ComboBox_task_1_5.SelectedValue == null)
+            {
+                return false;
+            }
+            else if (ComboBox_task_1_6.SelectedValue == null)
+            {
+                return false;
+            }
+            else if (ComboBox_task_1_7.SelectedValue == null)
+            {
+                return false;
+            }
+            else if (textBox_2_1.Text.Length == 0)
+            {
+                return false;
+            }
+            else if (textBox_2_2.Text.Length == 0)
+            {
+                return false;
+            }
+            else if (textBox_2_3.Text.Length == 0)
+            {
+                return false;
+            }
+            else if (textBox_2_4.Text.Length == 0)
+            {
+                return false;
+            }
+            else if (textBox_2_5.Text.Length == 0)
+            {
+                return false;
+            }
+            else if (textBox_3_1.Text.Length == 0)
+            {
+                return false;
+            }
+            else if (textBox_3_2.Text.Length == 0)
+            {
+                return false;
+            }
+            else if (textBox_3_3.Text.Length == 0)
+            {
+                return false;
+            }
+            else if (textBox_4_1.Text.Length == 0)
+            {
+                return false;
+            }
+            else if (textBox_4_2.Text.Length == 0)
+            {
+                return false;
+            }
+            else if (textBox_4_3.Text.Length == 0)
+            {
+                return false;
+            }
+            else if (textBox_4_4.Text.Length == 0)
+            {
+                return false;
+            }
+            else if (textBox_4_5.Text.Length == 0)
+            {
+                return false;
+            }
+
+            return false;
         }
     }
 }
