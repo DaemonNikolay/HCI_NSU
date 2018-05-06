d13b6ab058bd2513c836ff3db034942f351d993a
DaemonNikolay <nikolay-eckert@mail.ru>
2018-05-07 00:07:59 +0700

    Add delete user

--- TRiZBD/trizbd.local/DeleteUser.php
+++ TRiZBD/trizbd.local/DeleteUser.php
@@ -1,7 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 06.05.2018
- * Time: 20:33
- */
+
+require_once "Connection.php";
+
+
+$id = $_REQUEST['id'];
+$sql = "DELETE FROM user WHERE id=$id";
+
+$r = mysqli_query($link, $sql) or die("error delete");
+
+mysqli_close($link);
+
+?>
+
+<HTML>
+<HEAD>
+    <META HTTP-EQUIV='Refresh' CONTENT='0; URL=Index.php'>
+</HEAD>
--- TRiZBD/trizbd.local/EditUser.php
+++ TRiZBD/trizbd.local/EditUser.php
@@ -1,7 +1,2 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 06.05.2018
- * Time: 20:34
- */
+
