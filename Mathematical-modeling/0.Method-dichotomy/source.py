c02a6cba9fc4548fde3616af863d97955c088c0f
DaemonNikolay <nikolay-eckert@mail.ru>
2018-09-16 13:24:43 +0700

    Update code

--- Mathematical-modeling/0.Method-dichotomy/source.py
+++ Mathematical-modeling/0.Method-dichotomy/source.py
@@ -11,23 +11,25 @@ if __name__ == '__main__':
     a = decimal.Decimal(-4)
     b = decimal.Decimal(4)
     limit = 100
-    function_a = decimal.Decimal(-1)
-    function_b = decimal.Decimal(-1)
-    function_c = decimal.Decimal(-1)
-    module_ba = decimal.Decimal(-1)
-    c = decimal.Decimal(-1)
+
+    c = (a + b) / 2
+    function_a = function_of_number(a)
+    function_b = function_of_number(b)
+    function_c = function_of_number(c)
+    module_ba = abs(b - a)
 
     table = PrettyTable(["#", "A", "B", "C", "F(A)", "F(B)", "F(C)", "|B-A|"])
-    for i in range(0, limit):
-        if i != 0:
-            a = c if function_a * function_c > 0 else a
-            b = c if function_b * function_c > 0 else b
+    table.add_row([1 + 1, a, b, c, function_a, function_b, function_c, module_ba])
 
+    for i in range(1, limit):
+        a = c if function_a * function_c > 0 else a
+        b = c if function_b * function_c > 0 else b
         c = (a + b) / 2
         function_a = function_of_number(a)
         function_b = function_of_number(b)
         function_c = function_of_number(c)
         module_ba = abs(b - a)
+
         table.add_row([i + 1, a, b, c, function_a, function_b, function_c, module_ba])
 
     print(table)
